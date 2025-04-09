1ab6525240a16634338919c838ae9c7a1f25b412
Rustem Maldybekov <mrustem501@gmail.com>
2025-04-09 12:01:54 +0600

    Created Gameplay Timer

--- maze_game_final/Assets/Scripts/Gameplay/GameplayController.cs
+++ maze_game_final/Assets/Scripts/Gameplay/GameplayController.cs
@@ -1,27 +1,59 @@
 namespace Gameplay
 {
     using Collectable;
+    using System;
+    using System.Collections;
     using UnityEngine;
     using UnityEngine.SceneManagement;
 
     public class GameplayController : MonoBehaviour
     {
-        [SerializeField] protected string sceneName = default;
+        public event Action<TimeSpan> OnTimerChanged = delegate { };
+
+        [SerializeField] protected string bossSceneName = default;
+        [SerializeField] protected string loseSceneName = default;
+        [SerializeField, Min(0.0f)] protected float timeSeconds = 0.0f;
         [SerializeField] protected CollectablesDataSO collectablesData = default;
 
-        protected virtual void OnEnable()
+        protected TimeSpan currentTimerTime = default;
+        protected Coroutine currentCoroutine = default;
+        protected TimeSpan currentTimerSteps = TimeSpan.FromSeconds(1.0f);
+
+        protected virtual void Awake()
         {
-            collectablesData.OnCollectablesCollected += CheckForWin;
+            currentTimerTime = TimeSpan.FromSeconds(timeSeconds);
+            OnTimerChanged(currentTimerTime);
+            StartTimer();
         }
+
+        protected virtual void OnEnable()
+            => collectablesData.OnCollectablesCollected += LoadBossScene;
 
         protected virtual void OnDisable()
-        {
-            collectablesData.OnCollectablesCollected -= CheckForWin;
-        }
+            => collectablesData.OnCollectablesCollected -= LoadBossScene;
 
-        protected virtual void CheckForWin()
+        protected virtual void LoadBossScene()
+            => SceneManager.LoadScene(bossSceneName);
+
+        protected virtual void StartTimer()
+            => currentCoroutine = StartCoroutine(StartCountDown());
+
+        protected virtual void StopTimer()
+            => StopCoroutine(currentCoroutine);
+
+        protected IEnumerator StartCountDown()
         {
-            SceneManager.LoadScene(sceneName);
+            yield return new WaitForSeconds(1.0f);
+            currentTimerTime = currentTimerTime.Subtract(currentTimerSteps);
+            OnTimerChanged(currentTimerTime);
+
+            if (currentTimerTime.TotalSeconds > 0)
+            {
+                StartCoroutine(StartCountDown());
+            } else
+            {
+                SceneManager.LoadScene(loseSceneName);
+            }
         }
     }
 }
