13ef01be547b7a1a7a7e9a69ddc2af2d57fafd53
Rustem Maldybekov <mrustem501@gmail.com>
2025-04-11 00:53:44 +0600

    Changed Player Movement

--- maze_game_final/Assets/Scripts/Player/PlayerMovement.cs
+++ maze_game_final/Assets/Scripts/Player/PlayerMovement.cs
@@ -18,6 +18,9 @@ namespace Player
 
         protected Vector2 movementStep = default;
 
+        protected virtual void Awake()
+            => movementSpeed.ResetValue();
+
         public virtual void SubscribeJoystick(Joystick joystick)
         {
             joystick.OnJoystickChanged += MovePlayer;
@@ -32,7 +35,7 @@ namespace Player
 
         protected virtual void MovePlayer(Vector2 movementDir)
         {
-            movementStep = movementDir * movementSpeed.Value * Time.time;
+            movementStep = movementDir * movementSpeed.Value * Time.deltaTime;
             playerRb.MovePosition(playerRb.position + movementStep);
             OnPlayerMoved(movementDir);
         }
