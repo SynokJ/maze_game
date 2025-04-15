97d91af4e2d36d8e8e354790fe2b369d18b30fb6
Rustem Maldybekov <mrustem501@gmail.com>
2025-04-15 13:09:45 +0600

    Created LevelMap Texture Generate System

--- maze_game_final/Assets/Scripts/Map/LevelMap.cs
+++ maze_game_final/Assets/Scripts/Map/LevelMap.cs
@@ -4,11 +4,13 @@ namespace LevelMap
     using Level;
     using System.Collections.Generic;
     using UnityEngine;
+    using UnityEngine.UI;
 
     public class LevelMap : MonoBehaviour
     {
         [SerializeField] protected LevelController levelController = default;
         [SerializeField] protected CollectablesController controller = default;
+        [SerializeField] protected Image levelMapImage = default;
 
         protected virtual void OnEnable()
             => controller.OnCollectablesGenerated += DrawLevelMap;
@@ -17,23 +19,31 @@ namespace LevelMap
             => controller.OnCollectablesGenerated -= DrawLevelMap;
 
         protected virtual void DrawLevelMap(List<LevelCell> data)
+            => levelMapImage.material.mainTexture = GenerateTexture(data);
+
+        protected virtual Texture2D GenerateTexture(List<LevelCell> data)
         {
-            string strMap = DataToString(data);
-            Debug.Log(strMap);
+            Texture2D texture = new Texture2D(levelController.LevelWidth, levelController.LevelHeight);
+
+            foreach (LevelCell cell in data)
+            {
+                Color color = CalculateColor(cell);
+                texture.SetPixel((int)cell.Position.x, (int)cell.Position.y, color);
+            }
+
+            texture.Apply();
+            return texture;
         }
 
-        protected virtual string DataToString(List<LevelCell> data)
+        protected virtual Color CalculateColor(LevelCell cell)
         {
-            string res = default;
-            data.ForEach(x =>
+            switch (cell.State)
             {
-                res += x + " ";
-                if (x.Position.x == levelController.LevelWidth - 1)
-                {
-                    res += '\n';
-                }
-            });
-            return res;
+                case LevelCellState.unwalkable: return Color.white;
+                case LevelCellState.walkable: return Color.black;
+                case LevelCellState.collectables: return Color.yellow;
+                default: return Color.black;
+            }
         }
     }
 }
